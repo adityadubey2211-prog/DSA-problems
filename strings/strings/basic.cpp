7f6ae54b987aaf1880c880b03627c14eaa7b2a83
adityadubey2211prog <adityadubey2211@gmail.com>
2025-12-30 22:22:31 +0530

    strings started

---
+++ strings/strings/basic.cpp
@@ -0,0 +1,7 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string str = "Hello";
+    cout<<str;
+    return 0;
+}
---
+++ strings/strings/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str = "Hello";
+    for(char ch: str){
+        cout<<ch<<" ";
+    }
+    return 0;
+}
