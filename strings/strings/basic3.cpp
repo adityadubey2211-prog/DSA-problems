2acc7d1aa7794c4d015ae12f38372f8f45f45994
adityadubey2211prog <adityadubey2211@gmail.com>
2026-01-02 15:26:58 +0530

    palindrome

---
+++ strings/strings/basic3.cpp
@@ -0,0 +1,9 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string str1 = "Aditya";
+    string str2 = "Dubey";
+    string str3 = str1 + " " + str2;
+    cout<<str3<<endl<<"this process is known as concatenation";
+    return 0;
+}
