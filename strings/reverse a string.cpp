a6139dcfc4e6b2ee6f027f6ae829e46683df1244
adityadubey2211prog <adityadubey2211@gmail.com>
2026-01-03 18:48:48 +0530

    updated reverse

--- strings/reverse a string.cpp
+++ strings/reverse a string.cpp
@@ -3,15 +3,19 @@ using namespace std;
 string reverseString(string s) {
     int n = s.length();
     string ans = "";
+    reverse(s.begin(), s.end());
     for(int i = 0; i < n; i++) {
         string word = "";
         while(i < n && s[i] != ' '){
             word += s[i];
             i++;
         }
-        ans = word + " " + ans;
+        if(word.length() > 0){
+            reverse(word.begin(), word.end());
+            ans += ' '+word;
+        }
     }
-    return ans;
+    return ans.substr(1);
 }
 int main(){
     string s = "the sky is blue";
